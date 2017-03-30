7c5f4ef3d2ad7db20d55b29a7a832bc2d211472b
bdaoust <bdaoust.dev@gmail.com>
2017-03-29 21:44:51 -0400

    Implemented methods addCardCopies(), getNumbCopies(), and getCards() of the Deck class with accompanying tests.

--- app/src/main/java/org/bdaoust/project7capstone/Deck.java
+++ app/src/main/java/org/bdaoust/project7capstone/Deck.java
@@ -1,17 +1,24 @@
 package org.bdaoust.project7capstone;
 
+import android.support.annotation.NonNull;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 
 import io.magicthegathering.javasdk.resource.Card;
 
 public class Deck {
 
     private String mDeckName;
-    private ArrayList<Card> mCards;
+    private HashMap<Integer, Card> mCards;
+    private HashMap<Integer, Integer> mNumbCopies;
 
     public Deck(String deckName){
         mDeckName = deckName;
-        mCards = new ArrayList<>();
+        mCards = new HashMap<>();
+        mNumbCopies = new HashMap<>();
     }
 
     public String getDeckName(){
@@ -22,7 +29,20 @@ public class Deck {
         mDeckName = deckName;
     }
 
-    public void addCardCopies(Card card, int numb){
+    public void addCardCopies(@NonNull Card card, int numb){
+        int multiverseId;
+
+        multiverseId = card.getMultiverseid();
+        if(numb < 0){
+            throw new IllegalArgumentException("The number of copies must can't be negative");
+        }
+
+        if(mCards.containsKey(multiverseId)){
+            mNumbCopies.put(multiverseId, mNumbCopies.get(multiverseId) + numb);
+        } else {
+            mCards.put(multiverseId, card);
+            mNumbCopies.put(multiverseId,numb);
+        }
     }
 
     public void setCardCopies(Card card, int numb){
@@ -30,13 +50,28 @@ public class Deck {
     }
 
     public int getNumbCopies(int multiverseId){
-        return 0;
+        if(mCards.containsKey(multiverseId)){
+            return mNumbCopies.get(multiverseId);
+        } else {
+            return 0;
+        }
     }
 
     public void removeCardCopies(int multiverseId){
     }
 
     public ArrayList<Card> getCards(){
-        return mCards;
+        ArrayList<Card> sortedCards;
+
+        sortedCards = new ArrayList<>(mCards.values());
+
+        Collections.sort(sortedCards, new Comparator<Card>() {
+            @Override
+            public int compare(Card card1, Card card2) {
+                return card1.getName().compareTo(card2.getName());
+            }
+        });
+
+        return sortedCards;
     }
 }
--- app/src/test/java/org/bdaoust/project7capstone/DeckTests.java
+++ app/src/test/java/org/bdaoust/project7capstone/DeckTests.java
@@ -3,6 +3,11 @@ package org.bdaoust.project7capstone;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
+import io.magicthegathering.javasdk.api.CardAPI;
+import io.magicthegathering.javasdk.resource.Card;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -31,5 +36,67 @@ public class DeckTests {
         mDeck = new Deck(mDeckName);
         mDeck.setDeckName(testDeckName);
         assertEquals(testDeckName, mDeck.getDeckName());
+    }
+
+    @Test
+    public void testAddCardCopies(){
+        int multiverseId;
+        Card card1;
+        Card card2;
+        Card card3;
+        ArrayList<Card> cards;
+
+        mDeck = new Deck(mDeckName);
+        cards = mDeck.getCards();
+        assertEquals(0, cards.size());
+
+        multiverseId = 191089; //Lightning Bolt (Magic 2010)
+        card1 = CardAPI.getCard(multiverseId);
+        mDeck.addCardCopies(card1, 1);
+        cards = mDeck.getCards();
+        //Check all cards are in the collection and sorted alphabetically
+        assertEquals(1, cards.size());
+        assertEquals(card1, cards.get(0));
+        //Check that the Deck has the right number of copies for each card.
+        assertEquals(1, mDeck.getNumbCopies(191089));
+
+        multiverseId = 191076; //Fireball (Magic 2010)
+        card2 = CardAPI.getCard(multiverseId);
+        mDeck.addCardCopies(card2, 2);
+        cards = mDeck.getCards();
+        //Check all cards are in the collection and sorted alphabetically
+        assertEquals(2, cards.size());
+        assertEquals(card2, cards.get(0));
+        assertEquals(card1, cards.get(1));
+        //Check that the Deck has the right number of copies for each card.
+        assertEquals(1, mDeck.getNumbCopies(191089));
+        assertEquals(2, mDeck.getNumbCopies(191076));
+
+        multiverseId = 1824; //Maze of Ith (The Dark)
+        card3 = CardAPI.getCard(multiverseId);
+        mDeck.addCardCopies(card3, 3);
+        cards = mDeck.getCards();
+        //Check all cards are in the collection and sorted alphabetically
+        assertEquals(3, cards.size());
+        assertEquals(card2, cards.get(0));
+        assertEquals(card1, cards.get(1));
+        assertEquals(card3, cards.get(2));
+        //Check that the Deck has the right number of copies for each card.
+        assertEquals(1, mDeck.getNumbCopies(191089));
+        assertEquals(2, mDeck.getNumbCopies(191076));
+        assertEquals(3, mDeck.getNumbCopies(1824));
+
+        //Add more copies of Fireball (Magic 2010)
+        mDeck.addCardCopies(card2, 7);
+        cards = mDeck.getCards();
+        //Check all cards are in the collection and sorted alphabetically
+        assertEquals(3, cards.size());
+        assertEquals(card2, cards.get(0));
+        assertEquals(card1, cards.get(1));
+        assertEquals(card3, cards.get(2));
+        //Check that the Deck has the right number of copies for each card.
+        assertEquals(1, mDeck.getNumbCopies(191089));
+        assertEquals(9, mDeck.getNumbCopies(191076));
+        assertEquals(3, mDeck.getNumbCopies(1824));
     }
 }
