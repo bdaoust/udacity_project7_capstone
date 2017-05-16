18ea999d78af8d40967d5aee46b6badd60e71c4a
bdaoust <bdaoust.dev@gmail.com>
2017-05-15 22:47:23 -0400

    Moved EditDeckActivity's CustomAdapater into it's own class as EditCardListAdapter.

--- app/src/main/java/org/bdaoust/project7capstone/activities/EditDeckActivity.java
+++ app/src/main/java/org/bdaoust/project7capstone/activities/EditDeckActivity.java
@@ -10,22 +10,13 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-
-import org.bdaoust.project7capstone.data.Deck;
+import org.bdaoust.project7capstone.adapters.EditCardListAdapter;
 import org.bdaoust.project7capstone.R;
 import org.bdaoust.project7capstone.data.SampleDeck;
 import org.bdaoust.project7capstone.fragments.SearchCardsDialogFragment;
-
-import io.magicthegathering.javasdk.resource.Card;
 
 public class EditDeckActivity extends AppCompatActivity{
 
@@ -55,7 +46,7 @@ public class EditDeckActivity extends AppCompatActivity{
         mRecyclerView = (RecyclerView)findViewById(R.id.editCardsList);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-        mRecyclerView.setAdapter(new CustomAdapter(new SampleDeck()));
+        mRecyclerView.setAdapter(new EditCardListAdapter(this, new SampleDeck()));
 
         mSearchCardsFAB = (FloatingActionButton) findViewById(R.id.searchCardsFAB);
 
@@ -78,118 +69,4 @@ public class EditDeckActivity extends AppCompatActivity{
         return true;
     }
 
-
-    private class CustomAdapter extends RecyclerView.Adapter<CustomViewHolder> {
-        private Deck mDeck;
-
-        public CustomAdapter(Deck deck){
-            mDeck = deck;
-        }
-
-        @Override
-        public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            CustomViewHolder customViewHolder;
-            View view;
-
-            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_edit_card, parent, false);
-            customViewHolder = new CustomViewHolder(view);
-
-            return customViewHolder;
-        }
-
-        @Override
-        public void onBindViewHolder(final CustomViewHolder holder, int position) {
-            final Card card;
-
-            card = mDeck.getCards().get(position);
-            Glide.with(holder.itemView.getContext()).load(card.getImageUrl()).into(holder.cardImage);
-            holder.cardName.setText(card.getName());
-            holder.cardNumbCopies.setText(String.valueOf(mDeck.getNumbCopies(card.getMultiverseid())));
-            holder.setName.setText(String.valueOf(card.getSetName()));
-
-            holder.incrementButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int numbCopies;
-
-                    numbCopies = mDeck.getNumbCopies(card.getMultiverseid());
-                    if(numbCopies < 99){
-                        mDeck.addCardCopies(card, 1);
-                        numbCopies++;
-                        holder.cardNumbCopies.setText(String.valueOf(numbCopies));
-
-                        if(numbCopies == 99){
-                            holder.incrementButton.getDrawable().setTint(getResources().getColor(R.color.icon_button_disabled_tint_color));
-                        }
-
-                        if(numbCopies == 2){
-                            holder.decrementButton.getDrawable().setTint(getResources().getColor(R.color.icon_button_tint_color));
-                        }
-                    }
-                }
-            });
-
-
-            holder.decrementButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int numbCopies;
-
-                    numbCopies = mDeck.getNumbCopies(card.getMultiverseid());
-                    if(numbCopies > 1){
-                        numbCopies--;
-                        mDeck.setCardCopies(card, numbCopies);
-                        holder.cardNumbCopies.setText(String.valueOf(numbCopies));
-
-                        if(numbCopies == 1){
-                            holder.decrementButton.getDrawable().setTint(getResources().getColor(R.color.icon_button_disabled_tint_color));
-                        }
-
-                        if(numbCopies == 98){
-                            holder.incrementButton.getDrawable().setTint(getResources().getColor(R.color.icon_button_tint_color));
-                        }
-                    }
-                }
-            });
-
-            holder.deleteButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mDeck.removeCardCopies(card.getMultiverseid());
-                    notifyDataSetChanged();
-                }
-            });
-
-        }
-
-
-
-        @Override
-        public int getItemCount() {
-            return mDeck.getCards().size();
-        }
-    }
-
-    private class CustomViewHolder extends RecyclerView.ViewHolder{
-
-        public ImageView cardImage;
-        public TextView cardName;
-        public TextView cardNumbCopies;
-        public TextView setName;
-        public ImageButton decrementButton;
-        public ImageButton incrementButton;
-        public ImageButton deleteButton;
-
-        public CustomViewHolder(View itemView) {
-            super(itemView);
-
-            cardImage = (ImageView) itemView.findViewById(R.id.cardImage);
-            cardName = (TextView) itemView.findViewById(R.id.cardName);
-            cardNumbCopies = (TextView) itemView.findViewById(R.id.cardNumbCopies);
-            setName = (TextView) itemView.findViewById(R.id.setName);
-            decrementButton = (ImageButton) itemView.findViewById(R.id.actionDecrementCardCopies);
-            incrementButton = (ImageButton) itemView.findViewById(R.id.actionIncrementCardCopies);
-            deleteButton = (ImageButton) itemView.findViewById(R.id.actionDeleteCardCopies);
-        }
-    }
 }
---
+++ app/src/main/java/org/bdaoust/project7capstone/adapters/EditCardListAdapter.java
@@ -0,0 +1,135 @@
+package org.bdaoust.project7capstone.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import org.bdaoust.project7capstone.R;
+import org.bdaoust.project7capstone.data.Deck;
+
+import io.magicthegathering.javasdk.resource.Card;
+
+public class EditCardListAdapter extends RecyclerView.Adapter<EditCardListAdapter.EditCardItemViewHolder> {
+
+    private Context mContext;
+    private Deck mDeck;
+
+    public EditCardListAdapter(Context context, Deck deck){
+        mContext = context;
+        mDeck = deck;
+    }
+
+    @Override
+    public EditCardItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        EditCardItemViewHolder editCardItemViewHolder;
+        View view;
+
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_edit_card, parent, false);
+        editCardItemViewHolder = new EditCardItemViewHolder(view);
+
+        return editCardItemViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final EditCardItemViewHolder holder, int position) {
+        final Card card;
+
+        card = mDeck.getCards().get(position);
+        Glide.with(holder.itemView.getContext()).load(card.getImageUrl()).into(holder.cardImage);
+        holder.cardName.setText(card.getName());
+        holder.cardNumbCopies.setText(String.valueOf(mDeck.getNumbCopies(card.getMultiverseid())));
+        holder.setName.setText(String.valueOf(card.getSetName()));
+
+        holder.incrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numbCopies;
+
+                numbCopies = mDeck.getNumbCopies(card.getMultiverseid());
+                if(numbCopies < 99){
+                    mDeck.addCardCopies(card, 1);
+                    numbCopies++;
+                    holder.cardNumbCopies.setText(String.valueOf(numbCopies));
+
+                    if(numbCopies == 99){
+                        holder.incrementButton.getDrawable().setTint(mContext.getResources().getColor(R.color.icon_button_disabled_tint_color));
+                    }
+
+                    if(numbCopies == 2){
+                        holder.decrementButton.getDrawable().setTint(mContext.getResources().getColor(R.color.icon_button_tint_color));
+                    }
+                }
+            }
+        });
+
+
+        holder.decrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numbCopies;
+
+                numbCopies = mDeck.getNumbCopies(card.getMultiverseid());
+                if(numbCopies > 1){
+                    numbCopies--;
+                    mDeck.setCardCopies(card, numbCopies);
+                    holder.cardNumbCopies.setText(String.valueOf(numbCopies));
+
+                    if(numbCopies == 1){
+                        holder.decrementButton.getDrawable().setTint(mContext.getResources().getColor(R.color.icon_button_disabled_tint_color));
+                    }
+
+                    if(numbCopies == 98){
+                        holder.incrementButton.getDrawable().setTint(mContext.getResources().getColor(R.color.icon_button_tint_color));
+                    }
+                }
+            }
+        });
+
+        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDeck.removeCardCopies(card.getMultiverseid());
+                notifyDataSetChanged();
+            }
+        });
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mDeck.getCards().size();
+    }
+
+    class EditCardItemViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView cardImage;
+        TextView cardName;
+        TextView cardNumbCopies;
+        TextView setName;
+        ImageButton decrementButton;
+        ImageButton incrementButton;
+        ImageButton deleteButton;
+
+        EditCardItemViewHolder(View itemView) {
+            super(itemView);
+
+            cardImage = (ImageView) itemView.findViewById(R.id.cardImage);
+            cardName = (TextView) itemView.findViewById(R.id.cardName);
+            cardNumbCopies = (TextView) itemView.findViewById(R.id.cardNumbCopies);
+            setName = (TextView) itemView.findViewById(R.id.setName);
+            decrementButton = (ImageButton) itemView.findViewById(R.id.actionDecrementCardCopies);
+            incrementButton = (ImageButton) itemView.findViewById(R.id.actionIncrementCardCopies);
+            deleteButton = (ImageButton) itemView.findViewById(R.id.actionDeleteCardCopies);
+        }
+    }
+}
+
+
