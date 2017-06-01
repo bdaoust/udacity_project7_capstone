0a588d1e92b8f579b3e0a02fc409833c18516e12
bdaoust <bdaoust.dev@gmail.com>
2017-06-01 16:21:43 -0400

    Enabled Firebase data persistence.

---
+++ app/src/main/java/org/bdaoust/project7capstone/MTGManagerApplication.java
@@ -0,0 +1,18 @@
+package org.bdaoust.project7capstone;
+
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MTGManagerApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enabling Firebase data persistance, based on solution from
+        // cdkn7 (https://stackoverflow.com/questions/37448186/setpersistenceenabledtrue-crashes-app)
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
