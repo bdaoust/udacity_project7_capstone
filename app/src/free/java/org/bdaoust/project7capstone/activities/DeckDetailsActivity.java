e72a95edcab75ba5e1b955344da6622d10c7ebbe
bdaoust <bdaoust.dev@gmail.com>
2017-06-03 15:50:21 -0400

    Created BaseDeckDetailsActivity as a super class of DeckDetailsActivity.

--- app/src/free/java/org/bdaoust/project7capstone/activities/DeckDetailsActivity.java
+++ app/src/free/java/org/bdaoust/project7capstone/activities/DeckDetailsActivity.java
@@ -2,52 +2,23 @@ package org.bdaoust.project7capstone.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
-import org.bdaoust.project7capstone.fragments.DeckDetailsFragment;
 import org.bdaoust.project7capstone.R;
 
-public class DeckDetailsActivity extends AppCompatActivity{
+public class DeckDetailsActivity extends BaseDeckDetailsActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Toolbar toolbar;
-        ActionBar actionBar;
         AdView adView;
         AdRequest adRequest;
 
-        setContentView(R.layout.activity_deck_details);
-
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        actionBar = getSupportActionBar();
-
-        if(actionBar != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setTitle("[Deck Name]");
-        }
-
-        if(savedInstanceState == null){
-            FragmentManager fragmentManager;
-            FragmentTransaction fragmentTransaction;
-            DeckDetailsFragment deckDetailsFragment;
-
-            deckDetailsFragment = new DeckDetailsFragment();
-
-            fragmentManager = getSupportFragmentManager();
-            fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.deckDetailsContainer, deckDetailsFragment);
-            fragmentTransaction.commit();
-        }
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
 
         adView = (AdView) findViewById(R.id.adView);
         adRequest = new AdRequest.Builder().build();
--- app/src/paid/java/org/bdaoust/project7capstone/activities/DeckDetailsActivity.java
+++ app/src/paid/java/org/bdaoust/project7capstone/activities/DeckDetailsActivity.java
@@ -1,48 +1,5 @@
 package org.bdaoust.project7capstone.activities;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
-import org.bdaoust.project7capstone.fragments.DeckDetailsFragment;
-import org.bdaoust.project7capstone.R;
-
-public class DeckDetailsActivity extends AppCompatActivity{
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Toolbar toolbar;
-        ActionBar actionBar;
-
-        setContentView(R.layout.activity_deck_details);
-
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        actionBar = getSupportActionBar();
-
-        if(actionBar != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setTitle("[Deck Name]");
-        }
-
-        if(savedInstanceState == null){
-            FragmentManager fragmentManager;
-            FragmentTransaction fragmentTransaction;
-            DeckDetailsFragment deckDetailsFragment;
-
-            deckDetailsFragment = new DeckDetailsFragment();
-
-            fragmentManager = getSupportFragmentManager();
-            fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.deckDetailsContainer, deckDetailsFragment);
-            fragmentTransaction.commit();
-        }
-    }
+public class DeckDetailsActivity extends BaseDeckDetailsActivity {
 
 }
