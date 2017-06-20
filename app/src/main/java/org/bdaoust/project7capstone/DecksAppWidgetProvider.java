162f6ac65a0121c3cd38079b430c51db9760dc08
bdaoust <bdaoust.dev@gmail.com>
2017-06-20 00:01:32 -0400

    Implemented the DecksAppWidgetProvider and DecksWidgetService to display a list of the user's Decks (without the PieChart for the Deck colour breakdown given that Custom Views are not supported in Widgets).

--- app/src/main/java/org/bdaoust/project7capstone/DecksAppWidgetProvider.java
+++ app/src/main/java/org/bdaoust/project7capstone/DecksAppWidgetProvider.java
@@ -1,7 +1,30 @@
 package org.bdaoust.project7capstone;
 
-
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
 
 public class DecksAppWidgetProvider extends AppWidgetProvider{
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId: appWidgetIds) {
+            RemoteViews remoteViews;
+            Intent intent;
+
+            intent = new Intent(context, DecksWidgetService.class);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+
+            remoteViews = new RemoteViews(context.getPackageName(), R.layout.app_widget_decks);
+            remoteViews.setRemoteAdapter(R.id.decks, intent);
+            remoteViews.setEmptyView(R.id.decks, R.id.empty_view);
+
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+        }
+
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
 }
---
+++ app/src/main/java/org/bdaoust/project7capstone/DecksWidgetService.java
@@ -0,0 +1,211 @@
+package org.bdaoust.project7capstone;
+
+
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.bdaoust.project7capstone.firebasemodels.MTGDeckModel;
+import org.bdaoust.project7capstone.tools.MTGTools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DecksWidgetService extends RemoteViewsService{
+
+    private final static String TAG = "DecksWidgetService";
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new DeckRemoteViewsFactory(this.getApplicationContext());
+    }
+
+    private class DeckRemoteViewsFactory implements RemoteViewsFactory {
+
+        private Context mContext;
+        private FirebaseDatabase mFirebaseDatabase;
+        private DatabaseReference mReferenceUserRoot;
+        private DatabaseReference mReferenceDecks;
+        private ChildEventListener mOnDecksChildEventListener;
+        private List<MTGDeckModel> mtgDecks;
+
+
+        DeckRemoteViewsFactory(Context context){
+            mtgDecks = new ArrayList<>();
+            mContext = context;
+
+            mFirebaseDatabase = FirebaseDatabase.getInstance();
+            mReferenceUserRoot = MTGTools.createUserRootReference(mFirebaseDatabase, null);
+            mReferenceDecks = MTGTools.createDeckListReference(mReferenceUserRoot);
+
+            createListeners();
+        }
+
+        @Override
+        public void onCreate() {
+            mReferenceDecks.addChildEventListener(mOnDecksChildEventListener);
+            Log.d(TAG,"RemoteViewsFatory CREATED");
+        }
+
+        @Override
+        public void onDataSetChanged() {
+
+        }
+
+        @Override
+        public void onDestroy() {
+            mReferenceDecks.removeEventListener(mOnDecksChildEventListener);
+            Log.d(TAG,"RemoteViewsFatory DESTROYED");
+        }
+
+        @Override
+        public int getCount() {
+            return mtgDecks.size();
+        }
+
+        @Override
+        public RemoteViews getViewAt(int position) {
+            RemoteViews remoteViews;
+            MTGDeckModel mtgDeck;
+            String extraInfo;
+            String lastUpdated;
+            long lastUpdatedTimestamp;
+            int numbCards;
+
+            mtgDeck = mtgDecks.get(position);
+            numbCards = mtgDeck.getNumbCards();
+            lastUpdatedTimestamp = mtgDeck.getLastUpdatedTimestamp();
+            lastUpdated = DateUtils.formatDateTime(mContext, lastUpdatedTimestamp, DateUtils.FORMAT_SHOW_YEAR);
+            extraInfo = mContext.getResources().
+                    getQuantityString(R.plurals.deck_extra_info, numbCards, numbCards, lastUpdated);
+
+            remoteViews = new RemoteViews(mContext.getPackageName(), R.layout.app_widget_list_item_deck);
+            remoteViews.setTextViewText(R.id.deckName, mtgDecks.get(position).getName());
+            remoteViews.setTextViewText(R.id.deckExtraInfo, extraInfo);
+
+            return remoteViews;
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 1;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        private void createListeners() {
+            mOnDecksChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    MTGDeckModel mtgDeck;
+
+                    mtgDeck = dataSnapshot.getValue(MTGDeckModel.class);
+                    mtgDeck.setFirebaseKey(dataSnapshot.getKey());
+                    mtgDecks.add(mtgDeck);
+
+                    updateAllWidgets();
+                    Log.d(TAG, "Widget MTG Deck added: " + mtgDeck.getFirebaseKey());
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                    MTGDeckModel updatedMTGDeck;
+                    int position = -1;
+
+                    updatedMTGDeck = dataSnapshot.getValue(MTGDeckModel.class);
+                    updatedMTGDeck.setFirebaseKey(dataSnapshot.getKey());
+
+                    for(int i = 0; i < mtgDecks.size(); i++){
+                        MTGDeckModel mtgDeck;
+
+                        mtgDeck = mtgDecks.get(i);
+                        if(mtgDeck.getFirebaseKey().equals(updatedMTGDeck.getFirebaseKey())){
+                            position = i;
+                        }
+                    }
+
+                    if(position >= 0){
+                        mtgDecks.set(position, updatedMTGDeck);
+                    }
+
+                    updateAllWidgets();
+                    Log.d(TAG, "Widget MTG Deck updated: " + updatedMTGDeck.getFirebaseKey());
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    MTGDeckModel removedMTGDeck;
+                    int position = -1;
+
+                    removedMTGDeck = dataSnapshot.getValue(MTGDeckModel.class);
+                    removedMTGDeck.setFirebaseKey(dataSnapshot.getKey());
+
+                    for(int i = 0; i < mtgDecks.size(); i++){
+                        MTGDeckModel mtgDeck;
+
+                        mtgDeck = mtgDecks.get(i);
+                        if(mtgDeck.getFirebaseKey().equals(removedMTGDeck.getFirebaseKey())){
+                            position = i;
+                        }
+                    }
+
+                    if(position >= 0){
+                        mtgDecks.remove(position);
+                    }
+
+                    updateAllWidgets();
+                    Log.d(TAG, "Widget MTG Deck removed: " + removedMTGDeck.getFirebaseKey());
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            };
+        }
+
+        private void updateAllWidgets(){
+            ComponentName componentName;
+            AppWidgetManager appWidgetManager;
+            int[] appWidgetIds;
+
+            appWidgetManager = AppWidgetManager.getInstance(mContext);
+            componentName = new ComponentName(mContext, DecksAppWidgetProvider.class);
+            appWidgetIds = appWidgetManager.getAppWidgetIds(componentName);
+
+            for(int appWidgetId: appWidgetIds){
+                appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.decks);
+            }
+
+        }
+    }
+}
